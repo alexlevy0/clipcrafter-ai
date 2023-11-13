6f3c50a612e5b08d8cbac9f7edd278963f6fc883
Alex Lévy <alexlevy0@gmail.com>
2023-11-13 11:34:52 +0100

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "qlipappaada6b47": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger52a3ac74": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "qliplabel": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,116 @@
+"use client";
 import Image from 'next/image'
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import awsconfig from '../aws-exports';
+// import type { AppProps } from 'next/app'
+import { StorageManager } from '@aws-amplify/ui-react-storage'
+import { Button, ColorMode, defaultDarkModeOverride } from '@aws-amplify/ui-react'
+import { ThemeProvider, Theme } from '@aws-amplify/ui-react';
+import React from 'react';
+
+
+Amplify.configure(awsconfig);
+
+const theme: Theme = {
+  name: 'my-theme',
+  tokens: {
+    colors: {
+      font: {
+        primary: { value: '#008080' },
+        // ...
+      },
+    },
+  },
+};
+
+const processFile = ({ file, key }) => {
+  const fileParts = key.split('.');
+  const ext = fileParts.pop();
+  return {
+    file,
+    // This will prepend a unix timestamp
+    // to ensure all files uploaded are unique
+    key: `${Date.now()}${fileParts.join('.')}.${ext}`,
+  };
+}
 
 export default function Home() {
+  const [colorMode] = React.useState<ColorMode>('system');
+  const theme = {
+    name: 'my-theme',
+    overrides: [defaultDarkModeOverride],
+  };
+  const [files, setFiles] = React.useState({});
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <ThemeProvider theme={theme} colorMode={colorMode}>
+      <>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <StorageManager
+          isResumable
+          accessLevel='public'
+          acceptedFileTypes={['video/*']}
+          maxFileSize={10000} // TODO
+          maxFileCount={5}
+          processFile={processFile}
+          onFileRemove={({ key }) => {
+            setFiles((prevFiles) => {
+              return {
+                ...prevFiles,
+                [key]: undefined,
+              };
+            });
+          }}
+          onUploadError={(error, { key }) => {
+            setFiles((prevFiles) => {
+              return {
+                ...prevFiles,
+                [key]: {
+                  status: 'error',
+                },
+              };
+            });
+          }}
+          onUploadSuccess={({ key }) => {
+            setFiles((prevFiles) => {
+              return {
+                ...prevFiles,
+                [key]: {
+                  status: 'success',
+                },
+              };
+            });
+          }}
+          onUploadStart={({ key }) => {
+            setFiles((prevFiles) => {
+              return {
+                ...prevFiles,
+                [key]: {
+                  status: 'uploading',
+                },
+              };
+            });
+          }}
+          components={{
+            FilePicker({ onClick }) {
+              return (
+                <Button variation="primary" onClick={onClick}>
+                  Browse Files
+                </Button>
+              );
+            },
+          }}
         />
-      </div>
+        {Object.keys(files).map((key) => {
+          return files[key] ? (
+            <div style={{ color: 'white' }}>
+              {key}: {files[key].status}
+            </div>
+          ) : null;
+        })}
+      </>
+    </ThemeProvider>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  );
 }
