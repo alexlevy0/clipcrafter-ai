cb38a767edc343fc5b1edb3b549c6fab9299c283
Alex Lévy <alexlevy0@gmail.com>
2023-11-25 08:41:10 +0100

    go

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,53 +1,13 @@
+/* eslint-disable react/jsx-no-undef */
 "use client";
 import '@aws-amplify/ui-react/styles.css';
 import { StorageManager } from '@aws-amplify/ui-react-storage'
-import { Flex, Button, defaultDarkModeOverride } from '@aws-amplify/ui-react'
+import { Text, Flex, Button, defaultDarkModeOverride, Loader } from '@aws-amplify/ui-react'
 import { ThemeProvider } from '@aws-amplify/ui-react';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 // @ts-ignore:next-line
 import dynamic from 'next/dynamic';
 import { Auth, Storage } from 'aws-amplify';
-
-// @ts-ignore:next-line
-const ReactPlayer: ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
-
-const noop = async () => { undefined }
-
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
-const retry = async ({ fn = noop, retries = 60, delay = 1000, keepTrying = false, err = '' }) => {
-  const attempt = async (remainingRetries: number): Promise<any> => {
-    try {
-      const result = await fn();
-      if (keepTrying && remainingRetries > 0) {
-        await sleep(delay);
-        return attempt(remainingRetries - 1);
-      }
-      return result;
-    } catch (error) {
-      if (remainingRetries <= 0) {
-        throw err || error;
-      }
-      await sleep(delay);
-      return attempt(remainingRetries - 1);
-    }
-  };
-
-  return attempt(retries);
-}
-
-const getData = async (key: string, getProps: boolean = false) => {
-  const config = {
-    validateObjectExistence: true,
-    download: false, expires: 3600
-  }
-  const [name, format] = key.split('.')
-  const newKey = `${name}_edited.${format}`
-  if (getProps) {
-    return await Storage.getProperties(newKey)
-  }
-  return await Storage.get(newKey, config)
-}
 
 const Picker = (props: { onClick: any }) => {
   return (
@@ -62,37 +22,66 @@ const Picker = (props: { onClick: any }) => {
   )
 }
 
+// @ts-ignore:next-line
+const ReactPlayer: ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
+
+const noop = async () => { undefined }
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const retry = async ({ fn = noop, retries = 60, delay = 1000, err = '' }) => {
+  const attempt = async (remainingRetries: number, lastError = null): Promise<any> => {
+    if (remainingRetries <= 0) {
+      throw new Error(`Échec après ${retries} tentatives: ${err}\nDernière erreur: ${lastError}`);
+    }
+    try {
+      return await fn();
+    } catch (error) {
+      console.log(`Tentative échouée avec l'erreur: ${error}, réessayer...`);
+      await sleep(delay);
+      return attempt(remainingRetries - 1);
+    }
+  };
+
+  return attempt(retries);
+}
+
+const getData = async (key: string) => {
+  try {
+    const config = {
+      validateObjectExistence: true,
+      download: false, expires: 3600
+    }
+    const [name, format] = key.split('.')
+    const newKey = `${name}_edited.${format}`
+    return await Storage.get(newKey, config)
+  } catch (error) {
+    console.log('getData Err' + error);
+  }
+}
+
 export const Main = () => {
   const [url, setUrl] = useState('')
   const [status, setStatus] = useState('')
 
   const onSuccess = async ({ key = '' }) => {
     if (!key) return
-    const url = await retry({
-      fn: async () => {
-        await getData(key)
-      }
-    })
-      .catch(console.error)
+
+    // @ts-ignore
+    const url = await retry({ fn: async () => await getData(key) }).catch(console.error)
+    setStatus('Voilà!')
     setUrl(url)
-    const props = await retry({
-      keepTrying: true,
-      fn: async () => {
-        await getData(`${key}:status`, true)
-      }
-    })
-      .catch(console.error)
-    const { metadata: { status = '' } } = props || {}
-    setStatus(status)
-    console.log({ status });
   }
 
   const onResetVideoUrl = () => setUrl('')
+
+  const ref = useRef(null);
 
   return (
     <ThemeProvider theme={{ name: 'my-theme', overrides: [defaultDarkModeOverride] }} colorMode={'dark'}>
       <div>
         <StorageManager
+          ref={ref}
           isResumable
           accessLevel='public'
           acceptedFileTypes={['video/*']}
@@ -100,9 +89,35 @@ export const Main = () => {
           onUploadSuccess={onSuccess}
           onFileRemove={onResetVideoUrl}
           onUploadError={onResetVideoUrl}
-          onUploadStart={onResetVideoUrl}
+          onUploadStart={() => setStatus('Computing Video')}
           components={{ FilePicker({ onClick }) { return <Picker onClick={onClick} /> } }}
         />
+        <Flex
+          backgroundColor={'rgb(13, 25, 38)'}
+          paddingLeft={60}
+          paddingRight={60}
+          paddingTop={50}
+          direction="column" justifyContent="space-around">
+          <>
+            {status && status !== 'Voilà!' && <Loader
+              emptyColor={'rgb(13, 25, 38)'}
+              filledColor={'grey'}
+              variation="linear"
+            />}
+            <Text
+              variation="primary"
+              as="p"
+              lineHeight="1.5em"
+              fontWeight={600}
+              fontSize="2em"
+              fontStyle="normal"
+              width="50vw"
+            >
+              {!status ? 'Upload a file to get started' : status}
+            </Text>
+          </>
+        </Flex>
+
         <main style={{ backgroundColor: 'rgb(13, 25, 38)' }} className="flex min-h-screen flex-col items-center justify-between p-0">
           <ReactPlayer playing={true} controls={true} url={url} width={"100%"} />
         </main>
