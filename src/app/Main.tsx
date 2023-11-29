1c24af89012def7a33087a1bcf924e12358f9573
Alex Lévy <alexlevy0@gmail.com>
2023-11-29 11:13:43 +0100

    remove /post

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -91,7 +91,7 @@ export const Main = () => {
       const error = await stripe?.redirectToCheckout({
         lineItems: [{ price: "price_1OHZaRHjC5oFez5B3xJk2zRS", quantity: 1 }],
         mode: "subscription",
-        successUrl: "http://localhost:3000/post",
+        successUrl: "http://localhost:3000",
         cancelUrl: "http://localhost:3000/cancel",
       });
       console.log({ error });
